0068813ce4763a0fa757f9d5335de00ab48748d3
jietang89 <jie.tang@mavs.uta.edu>
2017-10-20 12:12:57 -0500

    Week 4 Update

--- Week 4/divide_and_conquer_solution_files/majority_element/majority_element.py
+++ Week 4/divide_and_conquer_solution_files/majority_element/majority_element.py
@@ -1,5 +1,6 @@
 # Uses python3
 import sys
+import random
 
 def get_majority_element(a, left, right):
     if left == right:
@@ -7,10 +8,21 @@ def get_majority_element(a, left, right):
     if left + 1 == right:
         return a[left]
     #write your code here
-
     return -1
+    
 
 
+def get_majority_element_sort(a, left, right):
+    a.sort()
+    mid = int((left+right)/2)
+    if a.count(a[mid]) > right/2: return 1
+    else: return -1
+
+# def get_majority_element_qsort(a, left, right):
+
+
+
+# out of memory limit
 def get_majority_element2(a,n):
     ma = 0
     for e in a:
@@ -25,19 +37,18 @@ def get_majority_element2(a,n):
         if count[e-1] > int(n/2): return e
     return -1
 
-
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     # n, *a = list(map(int, input().split()))
 
-    # if get_majority_element(a, 0, n) != -1:
-    #     print(1)
-    # else:
-    #     print(0)
-
-    if get_majority_element2(a, n) != -1:
+    if get_majority_element_sort(a, 0, n) != -1:
         print(1)
     else:
         print(0)
+
+    # if get_majority_element2(a, n) != -1:
+    #     print(1)
+    # else:
+    #     print(0)
     # get_majority_element2(a,n)
