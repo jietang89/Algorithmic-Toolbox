5da7683f1ee30d10cb12b3f1733f0cf3ed9f5155
jietang89 <jie.tang@mavs.uta.edu>
2017-10-27 14:02:48 -0500

    Week 5 Updates

--- Week 5/04_dynamic_programming_solution_files/placing_parentheses/placing_parentheses.py
+++ Week 5/04_dynamic_programming_solution_files/placing_parentheses/placing_parentheses.py
@@ -8,12 +8,15 @@ def evalt(a, b, op):
         return a * b
     else:
         assert False
-
-
+def MaxandMin (i,j):
+    for k in range(i,j):
+        a = evalt(MaxandMin(i,k),MaxandMin(k+1,j),op[k])
+        
 def get_maximum_value(dataset):
     #write your code here
     num = list(map(int,dataset[::2]))
     op = list(dataset[1::2])
+
     return num,op
 
 
